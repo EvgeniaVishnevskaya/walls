065337d9c271cb3fe3b88bd5e4296c2ae08c544d
EvgeniaVishnevskaya <55274706+EvgeniaVishnevskaya@users.noreply.github.com>
2021-09-06 23:21:04 +0300

    моя версия

--- js/sketch.js
+++ js/sketch.js
@@ -2,22 +2,24 @@ let walls = [];
 let ray;
 let particle;
 let xoff = 0;
-let yoff = 10000;
+let yoff = 1000;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < 4; i++){
     let x1 = random(width);
     let x2 = random(width);
     let y1 = random(height);
     let y2 = random(height);
     walls[i] = new Boundary(x1, y1, x2, y2);
   }
-  walls.push(new Boundary(-1, -1, width, -1));
-  walls.push(new Boundary(width, -1, width, height));
-  walls.push(new Boundary(width, height, -1, height));
-  walls.push(new Boundary(-1, height, -1, -1));
+  walls.push(new Boundary(0, 0, width, 0));
+  walls.push(new Boundary(width, 0, width, height));
+  walls.push(new Boundary(width, height, 0, height));
+  walls.push(new Boundary(0, height, 0, 0));
   particle = new Particle();
+  
+
 }
 
 function draw() {
@@ -25,11 +27,21 @@ function draw() {
   for (let wall of walls) {
     wall.show();
   }
-  //particle.update(noise(xoff) * width, noise(yoff) * height);
-  particle.update(mouseX, mouseY);
+  
+//   particle.update(noise(xoff) * width, noise(yoff) * height);
+  particle.update(mouseX, mouseY)
   particle.show();
   particle.look(walls);
 
-  xoff += 0.01;
-  yoff += 0.01;
+  xoff += 0.005;
+  yoff += 0.005;
+//   ray.show();
+//   ray.lookAt(mouseX, mouseY);
+
+//   let pt = ray.cast(wall);
+//   if (pt) {
+//       fill(255);
+//       ellipse(pt.x, pt.y, 8, 8);
+//   }
 }
+
